ed52b1ae51835006c34168173339fca9af553ddd
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-01-30 09:32:56 +0100

    Prueba

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-numSubdirectorios=$1
+numSubdirectorios=$1 #nos guardamos el parámetro en la variable
 
 if [ -z "$numSubdirectorios" ]; then #El -z comprueba si una cadena está vacía o no
     echo "No has introducido el parámetro, por favor introdúcelo"
@@ -8,7 +8,7 @@ fi
 
 while read nombre; do
 
-    mkdir -p "$nombre" 
+    mkdir -p "$nombre" # creamos directorios, la opcion -p, permite crear directorios sino los tiene
     for((i=1; i<=$numSubdirectorios; i++)); do
         mkdir -p "$nombre/Personal $i"
     done
