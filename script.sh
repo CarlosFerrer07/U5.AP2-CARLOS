3512db464dc5212a5fa12c323b995f91267a5526
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-01-30 09:38:32 +0100

    fixing

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-numSubdirectorios=$1 #nos guardamos el parámetro en la variable
+numSubdirectorios=$1 #nos guardamos el parámetro en la variable.
 
-if [ -z "$numSubdirectorios" ]; then #El -z comprueba si una cadena está vacía o no
+if [ -z "$numSubdirectorios" ]; then #El -z comprueba si una cadena está vacía o no.
     echo "No has introducido el parámetro, por favor introdúcelo"
 fi
 
-while read nombre; do
+while read nombre; do #Leemos mediante el bucle el archivo nombres.txt y guardamos cada nombre en variable nombre
 
     mkdir -p "$nombre" # creamos directorios, la opcion -p, permite crear directorios sino los tiene
     for((i=1; i<=$numSubdirectorios; i++)); do
